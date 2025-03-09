5e8003ff8cd6b461c950dd840980f8f25114e3f2
rossilor95 <rossilor95@outlook.it>
2025-03-09 19:25:51 +0100

    feat: add subset of token definitions

---
+++ src/main/kotlin/monkey/Token.kt
@@ -0,0 +1,27 @@
+package monkey
+
+sealed interface Token {
+    object Illegal : Token
+    object EndOfFile : Token
+
+    data class Identifier(val name: String) : Token
+    data class NumberLiteral(val value: Int) : Token
+    data class StringLiteral(val value: String) : Token
+
+    enum class Operator : Token {
+        ASSIGN, PLUS
+    }
+
+    enum class Delimiter : Token {
+        COMMA, LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, SEMICOLON,
+    }
+
+    enum class Keyword : Token {
+        FUNCTION, LET,
+    }
+}
+
+val keywordTable = mapOf(
+    "fn" to Token.Keyword.FUNCTION,
+    "let" to Token.Keyword.LET,
+)
