c7cc56ddfbe091f96bbc5e50d980ad9dfcbbd29b
rossilor95 <rossilor95@outlook.it>
2025-03-14 17:01:54 +0100

    feat: implement basic REPL

--- src/main/kotlin/monkey/Main.kt
+++ src/main/kotlin/monkey/Main.kt
@@ -1,6 +1,15 @@
 package monkey
 
+private const val PROMPT = ">> "
+
 fun main() {
-    val name = "Kotlin"
-    println("Hello, $name!")
+    println("Welcome to the Monkey programming language!")
+    println("Feel free to type in commands")
+
+    while (true) {
+        print(PROMPT)
+        val line = readlnOrNull() ?: continue
+        val lexer = Lexer(line)
+        lexer.forEachRemaining { println(it) }
+    }
 }
