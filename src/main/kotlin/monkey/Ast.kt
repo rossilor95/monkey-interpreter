2fabcd71821f1f5ba45e17943d56381decd185c0
rossilor95 <rossilor95@outlook.it>
2025-03-16 23:35:01 +0100

    feat: add AST structure with Node, Expr, and Stmt interfaces

---
+++ src/main/kotlin/monkey/Ast.kt
@@ -0,0 +1,13 @@
+package monkey
+
+sealed interface Node
+
+sealed interface Expr : Node {
+    data class Identifier(val value: String) : Expr
+}
+
+sealed interface Stmt : Node {
+    data class Let(val name: Expr.Identifier, val value: Expr?) : Stmt
+}
+
+data class Program(val statements: List<Stmt>) : Node
